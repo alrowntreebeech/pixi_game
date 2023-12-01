1f5b93f090e4963aa78ea372140a75295a203079
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-12-01 14:45:35 +0000

    Add enemies

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import {Assets, Application, Sprite} from "pixi.js";
+import {Assets, Application, Sprite, MeshBatchUvs} from "pixi.js";
 
 const app = new Application({
     width: 512,
@@ -13,7 +13,12 @@ app.renderer.view.style.display = "block";
 
 document.body.appendChild(app.view);
 
-let dungeon, explorer, treasure, door, id;
+let dungeon, explorer, treasure, door;
+
+// Helper function - useful to remember
+const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
 
 // Load Assets
 Assets.load([
@@ -31,6 +36,26 @@ Assets.load([
     treasure.x = app.stage.width - treasure.width - 48;
     treasure.y = app.stage.height / 2;
     app.stage.addChild(treasure);
+
+    const numberOfBlobs = 6;
+    const spacing = 48;
+    const xOffset = 150;
+
+    for (let i = 0; i < numberOfBlobs; i++) {
+
+        // Make a blob
+        const blob = Sprite.from("blob.png");
+
+        const x = spacing * i + xOffset;
+
+        const y = randomInt(0, app.stage.height - blob.height);
+
+        blob.x = x;
+        blob.y = y;
+
+        app.stage.addChild(blob);
+
+    }
 
     app.renderer.render(app.stage);
 });
